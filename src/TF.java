6c75fadaab23a090d7f94c5cd4b592906fc4fb2d
yariv zur <yarivtzur@gmail.com>
2012-11-18 02:05:40 +0200

    add

--- src/TF.java
+++ src/TF.java
@@ -14,13 +14,17 @@ import java.util.Map;
 public class TF {
 
 	private HashMap<String,Double> tfValues;
-	HashMap<String,HashMap<String, Double>> docstf;
+	private HashMap<String,HashMap<String, Double>> docstf;
 	int wordCount = 0;
 
+	//constructor for TF class
 	public TF(){
 		docstf = new HashMap<String,HashMap<String, Double>>();
 	}
 
+	/*
+	generate TF values for each word in each document and save it all in a HashMap of HashMaps
+	*/
 	public void calculateTF(String[] docs) {
 		 for(int j=0; j<docs.length;j++){
 			try{
@@ -31,43 +35,49 @@ public class TF {
 					DataInputStream in = new DataInputStream(fstream);
 					BufferedReader br = new BufferedReader(new InputStreamReader(in));
 					String strLine;
-					tfValues = new HashMap<String, Double>();
+					tfValues = new HashMap<String, Double>();   //initialize a new HashMap for the document tf values
 					//Read File Line By Line
 					while ((strLine = br.readLine()) != null)
 					{
-						strLine.toLowerCase();
-						String[] words = strLine.split("\\W");
+						strLine = strLine.toLowerCase();    //normalize all text to lowercase
+						String[] words = strLine.split("\\W");     //tokenize each line with everything not alphanumeric as delimiter
 						for (String s:words){
 							if(!s.isEmpty()){
-								if (tfValues.containsKey(s))
+								if (tfValues.containsKey(s)) //word has appeared before in doc
 								{
-									tfValues.put(s,tfValues.get(s)+1);
+									tfValues.put(s,tfValues.get(s)+1.0);  //aggregate word appearance counter
 								}else{
-									tfValues.put(s,1.0);
+									tfValues.put(s,1.0);   //word has not appeared before
 								}
-								wordCount++;
+								wordCount++;  //keep counter for total number of words in doc
 							}
 						}
 
 					}
+					docstf.put(docs[j],tfValues);
 					//Close the input stream
 					in.close();
 				}catch (Exception e){//Catch exception if any
 					System.err.println("Error: " + e.getMessage());
 				}
 			}
+		//iterate over HashMap and replace each word's count with its TF value
 		for(Map.Entry<String,Double> entry : tfValues.entrySet()){
 			entry.setValue(entry.getValue()/wordCount);
 		}
 	}
 
 	public double getTF(String doc,String word){
-		word = word.toLowerCase();
+		word = word.toLowerCase();     //normalize word to lowercase
 		HashMap<String, Double> tf = docstf.get(doc);
 		if(tfValues.containsKey(word)){
 			return tfValues.get(word);
 		}else{
 			return 0;
 		}
+	}
+
+	public HashMap<String,HashMap<String, Double>> getDocstf(){
+		return docstf;
 	}
 }
--- src/wdmAss.java
+++ src/wdmAss.java
@@ -1,6 +1,7 @@
 import java.io.FileNotFoundException;
 import java.util.Arrays;
 import java.util.Iterator;
+import java.util.Vector;
 
 /**
  * Created with IntelliJ IDEA.
@@ -13,28 +14,30 @@ public class wdmAss {
 
 	public static void main(String[] args) throws FileNotFoundException
 	{
+		int FLAG_OR = 0, FLAG_AND = 0;
 		double initPR = 1;					// Initial PageRank value
 		double epsilon = 0.01;				// The value for which we will stop trying to update a vertex's pageRank
 		double damping = 0.3;				// Damping Factor
         double startTime = System.currentTimeMillis();
 
 		String option = args[0];
-        String[] files = new String[args.length-1];
-		Arrays.copyOfRange(args,1,files.length);
+
 		if(option.equals("-graph")){
+			String[] files = Arrays.copyOfRange(args,1,args.length-1);
 			graph g = new graph();
-			g.generateGraph(args,initPR);  	// Creates the graph
+			g.generateGraph(files,initPR);  	// Creates the graph
 			g.clean(); 							// Cleans the graph from dead links
 			g.print();							// Prints the graph
 			return;
 		}
 		else if(option.equals("-pr")){
+			String[] files = Arrays.copyOfRange(args,1,args.length-1);
 			graph g = new graph();
-			g.generateGraph(args,initPR);  	// Creates the graph
+			g.generateGraph(files,initPR);  	// Creates the graph
 			g.clean(); 							// Cleans the graph from dead links
 			g.calcPr(epsilon,damping);			// Calculates PageRank for each vertex
 			double estimatedTime = System.currentTimeMillis() - startTime;
-			System.out.println("Execution time: "+estimatedTime);
+			System.out.println("Execution time: " + estimatedTime);
 			System.out.println();
 
 			//print
@@ -49,23 +52,55 @@ public class wdmAss {
 		else if(option.equals("-tf")){
 			TF tf = new TF();
 			String[] doc = new String[1];
-			doc[0] = args[2];
+			doc[0] = args[1];
 			tf.calculateTF(doc);
-			System.out.println(tf.getTF(doc[0],args[3]));
+			System.out.println(tf.getTF(doc[0],args[2]));
 			return;
 		}
 		else if(option.equals("-ta")){
+			Vector<String> words = new Vector<String>();
+			int k=0;
+			if(args[1].contains("(")){             //parse the word set and determine the boolean operator
+				words.add(args[1].substring(1).toLowerCase());
+				for(k=2;!args[k].contains(")");k++){
+					if(args[k].equals("OR")){
+						FLAG_OR = 1;
+					}
+					else if(args[k].equals("AND")){
+						FLAG_AND = 1;
+					}
+					else{
+						words.add(args[k].toLowerCase());
+					}
+				}
+				words.add(args[k].substring(0,args[k].indexOf(")")));   //add last word of the set
+			}
+			String[] files = Arrays.copyOfRange(args,k+1,args.length-1);
+			graph g = new graph();
+			g.generateGraph(files,initPR);  	// Creates the graph
+			g.clean(); 							// Cleans the graph from dead links
+			g.calcPr(epsilon,damping);			// Calculates PageRank for each vertex
+			TF tf = new TF();
+			tf.calculateTF(files);
+
+
+
+
+
+
+
+
+
+
 
 		}
 		else if(option.equals("-help")){
 			System.out.println("Options:\n-graph\tCreate a graph from the given files\n");
 			System.out.println("-help\tDisplay command line options\n");
 			System.out.println("-pr\tCalculate PageRank values for a graph generated from files\n");
-			System.out.println("-tf\tCalculate TF value for a word in a given file\n");
-			System.out.println("-ta\tRun Threshold Algorithm for a given set of words & documents and display most relevant documents\n");
-
-
-		}
+			System.out.println("-tf DOCUMENT WORD\tCalculate TF value for a word in a given file\n");
+			System.out.println("-ta (WORDS)\tRun Threshold Algorithm for a given set of words & documents and display most relevant documents\n");
+        }
 		else{
 			System.out.println("Usage: wmdAss.jar [options] file1 file2...");
 		}
